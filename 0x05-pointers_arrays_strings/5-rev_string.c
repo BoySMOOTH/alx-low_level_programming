81ede6d80ddc74b7c658c081f1ef1b2fac153b60
BoySMOOTH <adekunleodusola@gmail.com>
2023-10-16 23:36:30 -0700

    3 4 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - Reversing a string.
+ * @s: String input.
+ * Return: no return.
+ */
+void rev_string(char *s)
+{
+	char *begin = s;
+	char t;
+
+	/*Set pointer at the end*/
+	for (; *(s); s++)
+	{
+	}
+	s--; /*no null*/
+
+	/*Loop to change the letters*/
+	for (; s >= begin; s--)
+	{
+		t = *begin;
+		*begin = *s;
+		*s = t;
+		begin++;
+	}
+}
