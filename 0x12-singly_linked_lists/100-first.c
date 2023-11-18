f7de0266302a8c20ccdbd70fba44d9de371fdfa3
BoySMOOTH <adekunleodusola@gmail.com>
2023-11-18 05:58:51 -0800

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * before_main - printing a message before the main function is execute.
+ *
+ * Return: nothing.
+ */
+
+void before_main(void)
+{
+	printf("you're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
