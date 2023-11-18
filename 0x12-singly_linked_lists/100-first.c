53550550d28dc478837c4cb4a2e39e20c3b05a13
BoySMOOTH <adekunleodusola@gmail.com>
2023-11-18 06:11:23 -0800

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include "lists.h"
-
+void before_main(void) __attribute__((constructor));
 /**
  * before_main - printing a message before the main function is execute.
  *
  * Return: nothing.
  */
-
 void before_main(void)
 {
 	printf("you're beat! and yet, you must allow,\n");
