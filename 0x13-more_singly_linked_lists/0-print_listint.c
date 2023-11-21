9ad6dcf58a3dbe6d6770e2eef672763d1c9a1a4c
BoySMOOTH <adekunleodusola@gmail.com>
2023-11-21 14:46:58 -0800

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,10 +3,10 @@
 #include "lists.h"
 
 /**
- * Print_listint - Print elements of a singly linked list
- * @h: Pointer to a list.
- * Return: integers.
+ * print_listint - print all the elements of a listint_t list.
+ * @h: Pointer to the head of the list.
  *
+ * Return: the number of nodes.
  */
 
 size_t print_listint(const listint_t *h)
